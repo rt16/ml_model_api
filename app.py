c8e4860a725534256c5106a77b41c7beafd9616a
Aarti <ajayakumar@3cubetechmed.com>
2021-05-11 21:15:56 +0530

    405 error code solved

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET'])
 @cross_origin(supports_credentials=True)
 def predict():
     if lr:
